ba7cdbffcbd51a15ddae63bfefc583d43b151f72
Daggoot <taksin_tang51@hotmail.com>
2024-04-13 22:11:12 +0700

    Update Level design

---
+++ Assets/SuperTiled2Unity/Import/WaterTile.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="WaterTile" tilewidth="32" tileheight="32" tilecount="261" columns="29">
+ <image source="../../Assets/Tileset-Animated Terrains-8 frames-.png" width="928" height="288"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
