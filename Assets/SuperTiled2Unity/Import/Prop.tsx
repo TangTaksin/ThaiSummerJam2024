fc669410c05004b7bb61998a5ea7bb20e1a1ef02
Daggoot <taksin_tang51@hotmail.com>
2024-04-18 21:17:54 +0700

    quest flow done

---
+++ Assets/SuperTiled2Unity/Import/Prop.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Prop" tilewidth="32" tileheight="32" tilecount="1332" columns="36">
+ <image source="../../Assets/demonstration.png" width="1154" height="1210"/>
+</tileset>
